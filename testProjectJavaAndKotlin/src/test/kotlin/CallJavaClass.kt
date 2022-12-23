6c1a6b044d527578134270e6ea2ed63d2e24dbba
ZabolotskiiMV <zabolotskiimv@sinara-finance.ru>
2022-12-23 17:34:49 +0700

    a little bit worked

--- testProjectJavaAndKotlin/src/test/kotlin/CallJavaClass.kt
+++
@@ -1,9 +0,0 @@
-import org.junit.Assert.assertEquals
-import org.junit.Assert.assertTrue
-
-class CallJavaClass {
-  @org.junit.Test
-  fun testCallJavaFoo() {
-    assertEquals(12, Foo.getDefaultInstances().size)
-  }
-}
--- testProjectLite/src/test/java/LiteTest.java
+++
@@ -1,9 +0,0 @@
-import static org.junit.Assert.assertTrue;
-
-public class LiteTest {
-  @org.junit.Test
-  public void testLiteProtos() {
-    assertTrue(com.google.protobuf.GeneratedMessageLite.class.isAssignableFrom(
-            io.grpc.testing.Messages.SimpleRequest.class));
-  }
-}
